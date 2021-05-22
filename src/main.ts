7110393b5aae1f3b46ee254100450f9ba52ae058
7Mica <15919514+7Mica@users.noreply.github.com>
2021-05-22 10:32:44 -0600

    Fixed main.ts config.

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,19 @@
-import { ValidationPipe } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const logger = new Logger('bootstrap');
+
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
+  const port = configService.get('SERVER_PORT') || 3000;
 
   app.useGlobalPipes(new ValidationPipe());
 
-  await app.listen(3000);
+  await app.listen(port);
+  logger.log(`Application listening on port ${port}`);
 }
+
 bootstrap();
